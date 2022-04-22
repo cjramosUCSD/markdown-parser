54eb3575bd28a9a5980de49b0d0869fb38d267e6
cjramosUCSD <103283539+cjramosUCSD@users.noreply.github.com>
2022-04-22 13:39:31 -0700

    Test all 4 files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,10 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Files;
+import java.util.*;
+import java.nio.file.Path;
+import java.io.IOException;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
@@ -7,7 +12,35 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void test1(){
+    public void getLinksTest() throws IOException{
+        Path filePath = Path.of("C:/Users/pyroh/OneDrive/Documents/GitHub/markdown-parser1/test-file.md");
+        String contents = Files.readString(filePath);
+        ArrayList<String> links = new MarkdownParse().getLinks(contents);
+        assertEquals("some-thing.html",links.get(1));
+        
+    }
+
+    public void getLinksTest2() throws IOException{
+        Path filePath = Path.of("C:/Users/pyroh/OneDrive/Documents/GitHub/markdown-parser1/test2.md");
+        String contents = Files.readString(filePath);
+        ArrayList<String> links = new MarkdownParse().getLinks(contents);
+        assertEquals(null,links.get(0));
+        
+    }
+
+    public void getLinksTest3() throws IOException{
+        Path filePath = Path.of("C:/Users/pyroh/OneDrive/Documents/GitHub/markdown-parser1/test3.md");
+        String contents = Files.readString(filePath);
+        ArrayList<String> links = new MarkdownParse().getLinks(contents);
+        assertEquals(null,links.get(0));
+        
+    }
+
+    public void getLinksTest4() throws IOException{
+        Path filePath = Path.of("C:/Users/pyroh/OneDrive/Documents/GitHub/markdown-parser1/test4.md");
+        String contents = Files.readString(filePath);
+        ArrayList<String> links = new MarkdownParse().getLinks(contents);
+        assertEquals(null,links.get(0));
         
     }
 }
