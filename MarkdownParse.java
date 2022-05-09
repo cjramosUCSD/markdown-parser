349080ceeee70b403e73b790a0c50b4e46849b3a
Chester J Ramos <cs15lsp22afd@ieng6-203.ucsd.edu>
2022-05-08 21:38:10 -0700

    Markdownparser edited

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -51,7 +51,7 @@ public class MarkdownParse {
         return toReturn;
     }
 
-
+    //Adding for writeup 3 testing
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
